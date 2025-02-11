6c974a7400075bc28c8f01b95d3fe0db002412c4
corbin <corbinchristopher1@gmail.com>
2025-02-10 19:54:33 -0700

    add multiple nvim plugins

---
+++ .config/nvim/after/plugin/lsp.lua
@@ -0,0 +1,19 @@
+local lsp_zero = require('lsp-zero')
+
+local lsp_attach = function(client, bufnr)
+  -- this is where you enable features that only work
+  -- if there is a language server active in the file
+end
+
+lsp_zero.extend_lspconfig({
+  sign_text = true,
+  lsp_attach = lsp_attach,
+  capabilities = require('cmp_nvim_lsp').default_capabilities()
+})
+
+-- Replace the language servers listed here
+-- with the ones you have installed in your system
+require('lspconfig').rust_analyzer.setup({})
+require('lspconfig').java_language_server.setup({})
+require('lspconfig').clangd.setup({})
+require('lspconfig').pyright.setup({})
