7ff0c00a121a3c70be27b46e875c965b8faa9ec1
corbin <corbinchristopher1@gmail.com>
2025-02-27 17:08:50 -0700

    add relative and absolute line numbers

--- .config/nvim/lua/garnetraven/remap.lua
+++ .config/nvim/lua/garnetraven/remap.lua
@@ -1,6 +1,9 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+vim.wo.number = true
+vim.wo.relativenumber = true
+
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
